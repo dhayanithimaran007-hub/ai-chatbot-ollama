06a897c94fdb13588cfbc5e13dbf9e46c38bb010
Dhayanithimaran K <dhayanithimaran.14@gmail.com>
2026-02-02 18:06:36 +0530

    Create IMPERIUS.py
    Add local AI chatbot using Python and Ollama

---
+++ IMPERIUS.py
@@ -0,0 +1,21 @@
+import ollama
+messages=[]
+print("World's fastest online bot is online🦅")
+print("Give your commands🐧")
+print("Press Q to exit🐲")
+while True:
+    user_input=input("You:")
+    if user_input.lower() in ["exit","quit","q"]:
+        print("Thank you for accessing me😘")
+        break
+    messages.append({"role":"user","content":user_input})
+    messages=messages[-4:]
+    response=ollama.chat(
+        model="phi3",
+        messages=messages
+        )
+    bot_reply=response["message"]["content"]
+    messages.append({"role":"assistant","content":bot_reply})
+    print("Bot🤖:",bot_reply)
+
+    
